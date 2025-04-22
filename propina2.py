e18fd0576d6a1c3a751b59c2d252e59d1cfb71d6
Abrahan1194 <abrahan.1194@gmail.com>
2025-04-21 19:45:08 -0500

    Update propina2.py

--- propina2.py
+++ propina2.py
@@ -1,5 +1,5 @@
-cuenta = int(input("ingrese Total cuenta:50    "))
-Porcentaje_propina  = int(input("Que porcentaje de propina desea agregar 10%,15%, 20% :"))
+cuenta = float(input("ingrese Total cuenta:50    "))
+Porcentaje_propina  = float(input("Que porcentaje de propina desea agregar 10%,15%, 20% :"))
 if Porcentaje_propina == 10:
     total_cuenta = (cuenta*0.10)
     print (total_cuenta)
